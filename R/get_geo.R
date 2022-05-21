96f57c7d3ed57590021285d5d1cb33d7a9670bb9
yun <yunyunpp96@outlook.com>
2022-05-22 00:04:20 +0800

    return a complete ExpressionSet object

--- R/get_geo.R
+++ R/get_geo.R
@@ -30,6 +30,21 @@ parse_geo_gse <- function(file) {
         ignore.case = TRUE, value = TRUE
     )]])
     gpl_data <- get_geo_gpl(gpl_id)[[1]]
+
+    Biobase::ExpressionSet(
+        assayData = .matrix_data,
+        phenoData = Biobase::AnnotatedDataFrame(
+            data = meta_data$Sample[colnames(.matrix_data), , drop = FALSE]
+        ),
+        featureData = gpl_data$fdata[
+            rownames(.matrix_data), ,
+            drop = FALSE
+        ],
+        experimentData = Biobase::MIAME(
+            other = c(meta_data$Series, gpl_data$gpl_meta)
+        ),
+        annotation = gpl_id
+    )
 }
 
 get_geo_gpl <- function(ids, dest_dir = getwd()) {
@@ -41,7 +56,7 @@ get_geo_gpl <- function(ids, dest_dir = getwd()) {
             ),
             error = function(error) {
                 rlang::inform(
-                    "Annotation for ", id, " is not available, so will use GPL SOFT instead"
+                    "Annotation for ", id, " is not available, so will use GPL SOFT file instead."
                 )
                 download_geo_files(
                     ids = id, dest_dir = dest_dir,
@@ -55,49 +70,89 @@ get_geo_gpl <- function(ids, dest_dir = getwd()) {
 
 parse_geo_gpl <- function(file) {
     file_text <- read_lines(file)
+
+    # extract feature data
+    .fdata <- data.table::fread(
+        text = file_text[!grepl("^[!#]", file_text, fixed = FALSE)],
+        sep = "\t", header = TRUE, blank.lines.skip = TRUE,
+        na.strings = c("NA", "null", "NULL", "Null")
+    )
+    data.table::setDF(.fdata)
+    rownames(.fdata) <- .fdata[["ID"]]
+
+    # extract meta data and column data
+    .column_data <- extract_geo_meta_and_column_data(
+        file_text = file_text, pattern = "^#\\w*?",
+        sub_pattern = "#"
+    )
+    .meta_data <- extract_geo_meta_and_column_data(
+        file_text = file_text, pattern = "^!\\w*?",
+        sub_pattern = "!"
+    )
+    list(
+        fdata = Biobase::AnnotatedDataFrame(
+            data = .fdata,
+            varMetadata = data.frame(
+                labelDescription = unname(.column_data[colnames(.fdata)]),
+                row.names = colnames(.fdata)
+            )
+        ),
+        gpl_meta = .meta_data
+    )
 }
 
-extract_geo_meta_data <- function(file_text) {
+extract_geo_meta_and_column_data <- function(file_text, pattern = "^(!|#)\\w*?", sub_pattern = NULL) {
     .metadata <- data.table::fread(
         text = file_text[
-            grepl("^!\\w*?_", file_text, fixed = FALSE)
+            grepl(pattern, file_text, fixed = FALSE)
         ], sep = "\t", header = FALSE, blank.lines.skip = TRUE,
         na.strings = c("NA", "null", "NULL", "Null")
     )
-    .metadata <- .metadata[, V1 := sub("^!\\w*?_", "", V1)]
-    data.table::dcast(
-        data.table::melt(
-            .metadata,
-            id.vars = "V1",
-            variable.name = "variable"
-        ), variable ~ V1,
-        fun.aggregate = paste0, collapse = "; "
-    )[, .SD, .SDcols = !"variable"]
-}
-
-extract_geo_column_annot <- function(file_text) {
-    .annot_data <- data.table::fread(
-        text = file_text[
-            grepl("^#.*=", file_text, fixed = FALSE)
-        ], sep = "\t", header = FALSE, blank.lines.skip = TRUE,
-        na.strings = c("NA", "null", "NULL", "Null")
-    )
-    .annot_data <- .annot_data[, V1 := sub("^#", "", V1)]
-    
+    # if there only have one column, it should contain "=" string to split this
+    # character into names and values, Otherwise, the first column should be the
+    # names of these meta data
+    if (identical(ncol(.metadata), 1L)) {
+        .metadata <- .metadata[[1]][
+            grepl("=", .metadata[[1]], perl = TRUE)
+        ]
+        meta_data <- data.table::tstrsplit(.metadata, "\\s*=\\s*", perl = TRUE)
+        if (is.null(sub_pattern)) sub_pattern <- "^(!|#)"
+        structure(meta_data[[2]], names = sub(sub_pattern, "", meta_data[[1]]))
+    } else {
+        if (is.null(sub_pattern)) sub_pattern <- pattern
+        .metadata <- .metadata[, V1 := sub(pattern, "", V1)]
+        data.table::dcast(
+            data.table::melt(
+                .metadata,
+                id.vars = "V1",
+                variable.name = "variable"
+            ), variable ~ V1,
+            fun.aggregate = paste0, collapse = "; "
+        )[, .SD, .SDcols = !"variable"]
+    }
 }
 
 extract_geo_gse_meta_data <- function(file_text) {
     meta_data <- c("Series", "Sample")
     names(meta_data) <- meta_data
-    meta_data <- lapply(meta_data, extract_geo_meta_data)
+    meta_data <- lapply(
+        meta_data, function(x) {
+            extract_geo_meta_and_column_data(
+                file_text,
+                pattern = paste0("^!", x, "_")
+            )
+        }
+    )
     data.table::setDF(meta_data$Sample)
     rownames(meta_data$Sample) <- meta_data$Sample[["geo_accession"]]
     meta_data$Series <- as.list(meta_data$Series)
-    if ("sample_id" %in% names(meta_data$Series)) {
-        meta_data$Series$sample_id <- strsplit(
-            meta_data$Series$sample_id,
-            split = " |; ", fixed = FALSE
-        )[[1]]
+    for (x in c("sample_id", "pubmed_id", "platform_id")) {
+        if (x %in% names(meta_data$Series)) {
+            meta_data$Series[[x]] <- strsplit(
+                meta_data$Series[[x]],
+                split = ";?+ ", fixed = FALSE
+            )[[1]]
+        }
     }
     meta_data
 }
