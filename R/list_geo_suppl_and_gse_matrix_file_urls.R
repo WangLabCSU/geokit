2c5c3119622aec2ca57dfe4aebe4ffcd46ca92fa
yun <yunyunpp96@outlook.com>
2022-05-21 20:47:06 +0800

    list geo files for supplementary and gse matrix files

--- R/list_geo_suppl_and_gse_matrix_file_urls.R
+++ R/list_geo_suppl_and_gse_matrix_file_urls.R
@@ -1,18 +1,26 @@
-list_geo_suppl_file_urls <- function(url, id) {
+list_file_helper <- function(url, id) {
     xml_doc <- xml2::read_html(url)
-    suppl_file_name <- str_extract_all(
+    file_name <- str_extract_all(
         xml2::xml_text(xml_doc),
         "G\\S++"
     )
-    if (identical(length(suppl_file_name), 1L) && !length(suppl_file_name)) {
+    if (identical(length(file_name), 1L) && !length(file_name)) {
         return(NULL)
     }
-    file.path(url, suppl_file_name[[1]])
+    file.path(url, file_name[[1]])
+}
+list_geo_file_urls <- function(ids, file_type) {
+    file_type <- match.arg(file_type, c("matrix", "suppl"))
+    urls <- build_geo_ftp_urls(ids, file_type)
+    .mapply(function(url, id) {
+        list_file_helper(url)
+    }, list(urls, ids), NULL)
 }
 
-list_geo_suppl_file_urls_multi <- function(ids) {
-    urls <- build_geo_ftp_urls(ids, "suppl")
-    .mapply(function(url, id) {
-        list_geo_suppl_file_urls(url)
-    }, list(urls, ids), NULL)
+list_geo_suppl_file_urls <- function(ids) {
+    list_geo_file_urls(ids, "suppl")
+}
+
+list_geo_gse_matrix_urls <- function(ids) {
+    list_geo_file_urls(ids, "matrixx")
 }
