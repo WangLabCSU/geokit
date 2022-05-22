44661ef55d58fd505968470f56dd5ff466b598fd
yun <yunyunpp96@outlook.com>
2022-05-22 14:38:14 +0800

    add document

--- R/build_geo_ftp_urls.R
+++ R/build_geo_ftp_urls.R
@@ -1,4 +1,16 @@
-# Programmatic access to GEO-[help](https://ftp.ncbi.nlm.nih.gov/geo/README.txt)
+build_geo_ftp_urls <- function(ids, file_type) {
+    geo_type <- unique(substr(ids, 1L, 3L))
+    super_ids <- sub("\\d{1,3}$", "nnn", ids, perl = TRUE)
+    file.path(
+        geo_ftp,
+        parse_geo_type(geo_type),
+        super_ids, ids, file_type,
+        parse_file_names(ids, file_type, geo_type)
+    )
+}
+
+#' @references Programmatic access to GEO-[help](https://ftp.ncbi.nlm.nih.gov/geo/README.txt)
+#' @rdname get_geo
 geo_ftp <- "ftp://ftp.ncbi.nlm.nih.gov/geo/"
 
 parse_geo_type <- function(x) {
@@ -10,6 +22,7 @@ parse_geo_type <- function(x) {
     )
 }
 
+#' @section GEO file type reference table:
 #' |            type            | GDS | GSE | GPL | GSM |
 #' | :------------------------: | :-: | :-: | :-: | :-: |
 #' |        SOFT (soft)         |  o  |  o  |  o  |  x  |
@@ -18,6 +31,39 @@ parse_geo_type <- function(x) {
 #' |      Matrix (matrix)       |  x  |  o  |  x  |  x  |
 #' |     Annotation (annot)     |  x  |  x  |  o  |  x  |
 #' | Supplementaryfiles (suppl) |  x  |  o  |  o  |  o  |
+#' @rdname get_geo
+parse_file_names <- function(ids, file_type, geo_type) {
+    file_suffix <-
+        switch(geo_type,
+            GDS = switch(file_type,
+                soft = ".soft.gz",
+                soft_full = "_full.soft.gz"
+            ),
+            GSE = switch(file_type,
+                soft = "_family.soft.gz",
+                miniml = "_family.xml.tgz",
+                matrix = "/",
+                suppl = "/"
+            ),
+            GPL = switch(file_type,
+                annot = ".annot.gz",
+                miniml = "_family.xml.tgz",
+                soft = "_family.soft.gz",
+                suppl = "/"
+            ),
+            GSM = if (identical(file_type, "suppl")) "/" else NULL
+        )
+    if (is.null(file_suffix)) {
+        rlang::abort(
+            paste0(parse_geo_type(geo_type), " never own ", file_type, " file.")
+        )
+    }
+    if (!identical(file_suffix, "/")) {
+        paste0(ids, file_suffix)
+    } else {
+        file_suffix
+    }
+}
 
 # parse_file_switch_helper <- list(
 #     GDS = c(soft = ".soft.gz", soft_full = "_full.soft.gz"),
@@ -47,47 +93,3 @@ parse_geo_type <- function(x) {
 #     }
 #     paste0(file_type, "/", file_ids)
 # }
-
-parse_file_names <- function(ids, file_type, geo_type) {
-    file_suffix <-
-        switch(geo_type,
-            GDS = switch(file_type,
-                soft = ".soft.gz",
-                soft_full = "_full.soft.gz"
-            ),
-            GSE = switch(file_type,
-                soft = "_family.soft.gz",
-                miniml = "_family.xml.tgz",
-                matrix = "/",
-                suppl = "/"
-            ),
-            GPL = switch(file_type,
-                annot = ".annot.gz",
-                miniml = "_family.xml.tgz",
-                soft = "_family.soft.gz",
-                suppl = "/"
-            ),
-            GSM = if (identical(file_type, "suppl")) "/" else NULL
-        )
-    if (is.null(file_suffix)) {
-        rlang::abort(
-            paste0(parse_geo_type(geo_type), " never own ", file_type, " file")
-        )
-    }
-    if (!identical(file_suffix, "/")) {
-        paste0(ids, file_suffix)
-    } else {
-        file_suffix
-    }
-}
-
-build_geo_ftp_urls <- function(ids, file_type) {
-    geo_type <- substr(ids, 1, 3)
-    super_ids <- sub("\\d{1,3}$", "nnn", ids, perl = TRUE)
-    file.path(
-        geo_ftp,
-        parse_geo_type(geo_type),
-        super_ids, ids, file_type,
-        parse_file_names(ids, file_type, geo_type)
-    )
-}
