d5f7b6c033c264689777939aa7938ad747fe2434
yun <yunyunpp96@outlook.com>
2022-05-22 14:36:36 +0800

    add prompt when no file and fix error

--- R/list_geo_suppl_and_gse_matrix_file_urls.R
+++ R/list_geo_suppl_and_gse_matrix_file_urls.R
@@ -1,20 +1,25 @@
 #' @import xml2
-list_file_helper <- function(url, id) {
+list_file_helper <- function(url) {
     xml_doc <- xml2::read_html(url)
     file_name <- str_extract_all(
         xml2::xml_text(xml_doc),
         "G\\S++"
     )
-    if (identical(length(file_name), 1L) && !length(file_name)) {
+    if (identical(length(file_name), 1L) && !length(file_name[[1L]])) {
         return(NULL)
     }
-    file.path(url, file_name[[1]])
+    file.path(url, file_name[[1L]])
 }
 list_geo_file_urls <- function(ids, file_type) {
-    file_type <- match.arg(file_type, c("matrix", "suppl"))
     urls <- build_geo_ftp_urls(ids, file_type)
     .mapply(function(url, id) {
-        list_file_helper(url)
+        res <- list_file_helper(url)
+        if (is.null(res)) {
+            rlang::inform(
+                paste0("No ", file_type, " files found for ", id, ".")
+            )
+        }
+        res
     }, list(urls, ids), NULL)
 }
 
