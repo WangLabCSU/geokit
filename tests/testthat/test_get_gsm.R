c070cbfe597453085bc56c0074c6f73fddd42837
yun <yunyunpp96@outlook.com>
2022-06-06 02:50:27 +0800

    test `get_geo` for GSM entity

---
+++ tests/testthat/test_get_gsm.R
@@ -0,0 +1,15 @@
+testthat::test_that("basic GSM works", {
+    gsm <- rgeo::get_geo("GSM11805", tempdir())
+
+    testthat::expect_s4_class(gsm, "GSM")
+    testthat::expect_s4_class(gsm, "GEODataTable")
+    testthat::expect_s4_class(gsm, "GEOData")
+    testthat::expect_type(meta(gsm), "list")
+    testthat::expect_s3_class(columns(gsm), "data.frame")
+    testthat::expect_s3_class(datatable(gsm), "data.frame")
+    testthat::expect_equal(accession(gsm), "GSM11805")
+    testthat::expect_equal(nrow(datatable(gsm)), 22283L)
+    testthat::expect_length(meta(gsm), 30L)
+    testthat::expect_equal(ncol(columns(gsm)), 1L)
+    testthat::expect_equal(nrow(columns(gsm)), 3L)
+})
