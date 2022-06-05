bbe86c6a24e1da711ab1b8bceded02b06f3fc026
yun <yunyunpp96@outlook.com>
2022-06-06 02:14:25 +0800

    test `get_geo` for `GPL` GEO entity

---
+++ tests/testthat/test_get_gpl.R
@@ -0,0 +1,38 @@
+testthat::test_that("generic GPL parsing works as expected", {
+    gpl <- rgeo::get_geo("GPL96", tempdir())
+
+    testthat::expect_s4_class(gpl, "GPL")
+    testthat::expect_s4_class(gpl, "GEODataTable")
+    testthat::expect_s4_class(gpl, "GEOData")
+    testthat::expect_equal(nrow(datatable(gpl)), 22283L)
+    testthat::expect_equal(ncol(datatable(gpl)), 21L)
+    testthat::expect_type(meta(gpl), "list")
+    testthat::expect_s3_class(datatable(gpl), "data.frame")
+})
+
+testthat::test_that("quoted GPL works", {
+    gpl <- rgeo::get_geo("GPL4133", tempdir())
+
+    testthat::expect_s4_class(gpl, "GPL")
+    testthat::expect_s4_class(gpl, "GEODataTable")
+    testthat::expect_s4_class(gpl, "GEOData")
+    testthat::expect_equal(nrow(datatable(gpl)), 45220L)
+})
+
+testthat::test_that("short GPL works", {
+    gpl <- rgeo::get_geo("GPL15505", tempdir())
+
+    testthat::expect_s4_class(gpl, "GPL")
+    testthat::expect_s4_class(gpl, "GEODataTable")
+    testthat::expect_s4_class(gpl, "GEOData")
+    testthat::expect_equal(nrow(datatable(gpl)), 52L)
+})
+
+testthat::test_that("GPL with no data table works", {
+    gpl <- rgeo::get_geo("GPL5082", tempdir())
+
+    testthat::expect_s4_class(gpl, "GPL")
+    testthat::expect_s4_class(gpl, "GEODataTable")
+    testthat::expect_s4_class(gpl, "GEOData")
+    testthat::expect_equal(nrow(datatable(gpl)), 0L)
+})
