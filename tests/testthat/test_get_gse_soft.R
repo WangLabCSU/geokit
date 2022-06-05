d61cbd017e1345d2807970f5db003149de1a566d
yun <yunyunpp96@outlook.com>
2022-06-06 03:25:31 +0800

    test `get_geo` for GSE entity with `gse_matrix = FALSE`

---
+++ tests/testthat/test_get_gse_soft.R
@@ -0,0 +1,14 @@
+testthat::test_that("generic SOFT format GSE handled correctly", {
+    gse <- rgeo::get_geo("GSE1563", tempdir(), gse_matrix = FALSE)
+
+    testthat::expect_equal(length(gsm(gse)), 62L)
+    testthat::expect_equal(length(gpl(gse)), 1L)
+    testthat::expect_equal(nrow(datatable(gpl(gse)[[1L]])), 12625L)
+    testthat::expect_equal(nrow(datatable(gsm(gse)[[1L]])), 12625L)
+    lapply(gsm(gse), function(x) {
+        testthat::expect_s4_class(x, "GSM") 
+        testthat::expect_equal(nrow(datatable(x)), 12625L)
+    }) 
+    testthat::expect_s4_class(gpl(gse)[[1L]], "GPL")
+    testthat::expect_equal(accession(gse), "GSE1563")
+})
