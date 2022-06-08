09d7c4d3f36090a5d1c970b059769ee4f2a6b055
yun <yunyunpp96@outlook.com>
2022-06-08 13:39:21 +0800

    add `show_geo` function to a HTML Browser

---
+++ R/show_geo.R
@@ -0,0 +1,24 @@
+#' Load GEO Accession site into a HTML Browser
+#' 
+#' @param id A character string representing the GEO entity
+#' ('GDS505','GSE2','GSM2','GPL96' eg.).
+#' @param browser a non-empty character string giving the name of the program to
+#' be used as the HTML browser. It should be in the PATH, or a full path
+#' specified. Alternatively, an R function to be called to invoke the
+#' browser.
+#'
+#' Under Windows NULL is also allowed (and is the default), and implies
+#' that the file association mechanism will be used.
+#' @details See [utils::browseURL()]
+#' @export
+show_geo <- function(id, browser = getOption("browser")) {
+    check_ids(id)
+    id <- toupper(id[[1]])
+    utils::browseURL(
+        build_geo_acc_url(
+            id,
+            scope = "self", amount = "quick", format = "html"
+        ),
+        browser = browser
+    )
+}
