3b15ec4d81f21625070d7dcdcd5370621c39420d
yun <yunyunpp96@outlook.com>
2022-07-23 11:43:34 +0800

    fix wrong when "Submitter supplied" is Nonexistent

--- R/search_geo.R
+++ R/search_geo.R
@@ -2,7 +2,7 @@
 #'
 #' This function searchs [GDS](https://www.ncbi.nlm.nih.gov/gds) database,
 #' and return a data.frame for all the search results.
-#' 
+#'
 #' The NCBI allows users to access more records (10 per second) if they register
 #' for and use an API key. [set_entrez_key][rentrez::set_entrez_key] function
 #' allows users to set this key for all calls to rentrez functions during a
@@ -10,7 +10,7 @@
 #' by [Sys.setenv][base::Sys.setenv].  Once this value is set to your key
 #' rentrez will use it for all requests to the NCBI. Details see
 #' <https://docs.ropensci.org/rentrez/articles/rentrez_tutorial.html#rate-limiting-and-api-keys>
-#' 
+#'
 #' @param query character, the search term. The NCBI uses a search term syntax
 #' which can be associated with a specific search field with square brackets.
 #' So, for instance "Homo sapiens\[ORGN\]" denotes a search for `Homo sapiens`
@@ -23,7 +23,7 @@
 #' @return a data.frame contains the search results
 #' @examples
 #' rgeo::search_geo("diabetes[ALL] AND Homo sapiens[ORGN] AND GSE[ETYP]")
-#' @export 
+#' @export
 search_geo <- function(query, step = 500L) {
     records_num <- rentrez::entrez_search(
         "gds", query,
@@ -48,6 +48,18 @@ search_geo <- function(query, step = 500L) {
         "\\n\\n"
     )[[1L]]
     name_value_pairs <- parse_name_value_pairs(preprocess_records(records))
+    tail_col <- c(
+        intersect(
+            c("Contains", "Datasets", "Series", "Platforms"),
+            names(name_value_pairs)
+        ),
+        grep("Accession$", names(name_value_pairs), perl = TRUE, value = TRUE)
+    )
+    data.table::setDT(name_value_pairs)
+    data.table::setcolorder(
+        name_value_pairs, 
+        tail_col, after = ncol(name_value_pairs)
+    )
     data.table::setDF(name_value_pairs)
     name_value_pairs
 }
@@ -57,22 +69,21 @@ search_geo <- function(query, step = 500L) {
 preprocess_records <- function(x) {
     x <- sub("^\\d+\\.", "Title:", x, perl = TRUE)
     x <- sub(
-        "\\n\\(Submitter supplied\\)\\s*",
-        "\nSummary: ", x,
+        "(Title:[^\\n]*\\n)(?:\\(Submitter supplied\\))?\\s*",
+        "\\1Summary: ", x,
         perl = TRUE
     )
     x <- gsub(
-        "(Platform|Dataset)s?:\\s*((?:GPL\\d+\\s*|GDS\\d+\\s*)+)",
+        "(Platform|Dataset|Serie)s?: *((?:GPL\\d+ *|GDS\\d+ *|GSE\\d+ *)+)",
         "\\1s: \\2\n", x,
         perl = TRUE
     )
     x <- sub("\\tID:\\s*", "\nID: ", x, perl = TRUE)
     x <- sub(
-        "((?:\\d+(?:\\s*related)? *(?:DataSet|Platform|Sample|Serie)s?\\s*)+)([^:])",
-        "Contains: \\1\\2", x,
+        "\\n((\\d+( Related| related)? (DataSet|Platform|Sample|Serie)s? *)+)\\n",
+        "\nContains: \\1\n", x,
         perl = TRUE
     )
-    x <- gsub(":\\t+", ": ", x, perl = TRUE)
-    x <- gsub("\\t\\t+", " ", x, perl = TRUE)
-    strsplit(x, "\\n", perl = TRUE)
+    x <- gsub("\\t+", " ", x, perl = TRUE)
+    strsplit(x, "\\n\\n?", perl = TRUE)
 }
