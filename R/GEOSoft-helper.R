ddc9eb3072d4bc046e3d4b46e2e9af8a35fb2e9d
yun <yunyunpp96@outlook.com>
2023-03-08 20:25:55 +0800

    add function get_geo_meta

--- R/GEOSoft-helper.R
+++ R/GEOSoft-helper.R
@@ -1,6 +1,36 @@
 # For GPL, GSM, and GDS entity, return a `GEOSoft` object
 # For GSE entity, return a `GEOSeries` object
-get_geo_soft <- function(id, geo_type, dest_dir = getwd(), curl_handle = NULL) {
+get_geo_soft <- function(id, geo_type, dest_dir, curl_handle) {
+    soft_data <- get_and_parse_soft(
+        id = id, geo_type = geo_type,
+        dest_dir = dest_dir, curl_handle = curl_handle,
+        only_meta = FALSE
+    )
+    new_geo_obj(id = id, geo_type = geo_type, soft_data = soft_data)
+}
+
+new_geo_obj <- function(id, geo_type, soft_data) {
+    switch(geo_type,
+        GSM = ,
+        GPL = ,
+        GDS = methods::new(
+            "GEOSoft",
+            meta = soft_data$meta,
+            columns = soft_data$columns,
+            datatable = soft_data$data_table,
+            accession = id
+        ),
+        GSE = methods::new(
+            "GEOSeries",
+            meta = soft_data$meta,
+            gsm = soft_data$gsm,
+            gpl = soft_data$gpl,
+            accession = id
+        )
+    )
+}
+
+get_and_parse_soft <- function(id, geo_type, dest_dir, curl_handle, only_meta) {
     file_path <- switch(geo_type,
         GSM = download_gsm_file(id,
             dest_dir = dest_dir,
@@ -21,28 +51,13 @@ get_geo_soft <- function(id, geo_type, dest_dir = getwd(), curl_handle = NULL) {
         )
     )
     file_text <- read_lines(file_path)
-    soft_data <- switch(geo_type,
-        GSM = ,
-        GPL = parse_gpl_or_gsm_soft(file_text),
-        GSE = parse_gse_soft(file_text, entity_type = "all"),
-        GDS = parse_gds_soft(file_text)
-    )
     switch(geo_type,
         GSM = ,
-        GPL = ,
-        GDS = methods::new(
-            "GEOSoft",
-            meta = soft_data$meta,
-            columns = soft_data$columns,
-            datatable = soft_data$data_table,
-            accession = id
+        GPL = parse_gpl_or_gsm_soft(file_text, only_meta = only_meta),
+        GSE = parse_gse_soft(file_text,
+            entity_type = "all",
+            only_meta = only_meta
         ),
-        GSE = methods::new(
-            "GEOSeries",
-            meta = soft_data$meta,
-            gsm = soft_data$gsm,
-            gpl = soft_data$gpl,
-            accession = id
-        )
+        GDS = parse_gds_soft(file_text, only_meta = only_meta)
     )
 }
---
+++ R/get_geo_meta.R
@@ -0,0 +1,33 @@
+#' Get the metadata of multiple GEO identities
+#'
+#' This is useful to combine with [search_geo()] and filter results since
+#' `search_geo()` cannot get all long metadata of GEO identities.
+#'
+#' @inheritParams get_geo
+#' @return A data.frame contains metadata of all ids.
+#' @export 
+get_geo_meta <- function(ids, curl_handle = NULL) {
+    res <- lapply(ids, function(id) {
+        out <- rlang::try_fetch(
+            get_and_parse_soft(
+                id = id, 
+                geo_type = substr(id, 1L, 3L),
+                dest_dir = tempdir(), 
+                curl_handle = curl_handle,
+                only_meta = TRUE
+            ),
+            error = function(err) {
+                rlang::abort(
+                    paste0("Error when fetching GEO metadata of ", id, "."),
+                    parent = err
+                )
+            }
+        )
+        out <- lapply(out$meta, function(x) {
+            if (length(x) == 1L) x else paste0(x, collapse = "; ")
+        })
+        data.table::setDT(out)
+    })
+    res <- data.table::rbindlist(res, use.names = TRUE, fill = TRUE)
+    data.table::setDF(res)
+}
